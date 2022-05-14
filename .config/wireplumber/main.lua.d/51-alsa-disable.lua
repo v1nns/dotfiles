8f32b385a783ecc5433b6aa3068d5683a94b93dc
Vinicius Longaray <vmouralongaray@gmail.com>
2022-05-14 17:33:37 -0300

    Include more rules to wireplumber lua script

--- .config/wireplumber/main.lua.d/51-alsa-disable.lua
+++ .config/wireplumber/main.lua.d/51-alsa-disable.lua
@@ -1,17 +1,33 @@
 -- INFO: Use 'wpctl' command to set default sink/source and volume for each one
 rule_nvidia = {
-    matches = {{{"device.name", "equals","alsa_card.pci-0000_01_00.1"}}},
+    matches = {{{"device.name", "equals", "alsa_card.pci-0000_01_00.1"}}},
     apply_properties = {
         ["device.disabled"] = true
     }
 }
 
 rule_webcam_mic = {
-    matches = {{{"device.name", "equals","alsa_card.usb-046d_Logitech_Webcam_C925e_3E5E5DCF-02"}}},
+    matches = {{{"device.name", "equals", "alsa_card.usb-046d_Logitech_Webcam_C925e_3E5E5DCF-02"}}},
     apply_properties = {
         ["device.disabled"] = true
     }
 }
 
+rule_dac_input = {
+    matches = {{{"node.name", "equals", "alsa_input.usb-FX-AUDIO-DAC-X6_FX-AUDIO-DAC-X6-01.analog-stereo"}}},
+    apply_properties = {
+        ["node.disabled"] = true
+    }
+}
+
+rule_dac_name = {
+    matches = {{{"node.name", "equals", "alsa_output.usb-FX-AUDIO-DAC-X6_FX-AUDIO-DAC-X6-01.analog-stereo"}}},
+    apply_properties = {
+        ["node.description"] = "DAC FX-AUDIO X6"
+    }
+}
+
 table.insert(alsa_monitor.rules, rule_nvidia)
 table.insert(alsa_monitor.rules, rule_webcam_mic)
+table.insert(alsa_monitor.rules, rule_dac_input)
+table.insert(alsa_monitor.rules, rule_dac_name)
