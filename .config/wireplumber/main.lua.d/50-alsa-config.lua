23fc2a774f24c1c18f6831764d5d6b2d4c71f9e1
Vinicius Longaray <vmouralongaray@gmail.com>
2022-05-23 15:06:52 -0300

    Improve rules on wireplumber, use F32 on librespot

--- .config/wireplumber/main.lua.d/50-alsa-config.lua
+++ .config/wireplumber/main.lua.d/50-alsa-config.lua
@@ -1,4 +1,4 @@
--- Matches all sources and sinks. This is necessary to avoid those "Broken pipe" errors
+-- Matches all sources and sinks. This is necessary to avoid "Broken pipe" errors
 golden_rule = {
     matches = {{{"node.name", "matches", "alsa_input.*"}}, {{"node.name", "matches", "alsa_output.*"}}},
     apply_properties = {
@@ -7,4 +7,25 @@ golden_rule = {
     }
 }
 
+--  Matches all sources and sinks except Built-in audio. This is necessary to avoid
+-- "spa.alsa: hw:X,0: Channels doesn't match (requested 64, got 2)" errors
+fix_channels = {
+    matches = {{{"node.name", "not-equals", "alsa_input.pci-0000_00_1f.3.capture.*"}},
+               {{"node.name", "not-equals", "alsa_output.pci-0000_00_1f.3.playback.*"}}},
+    apply_properties = {
+        ["audio.channels"] = 2
+    }
+}
+
+-- This rule is to instantly use DAC right after connected
+switch_to_dac = {
+    matches = {{{"node.name", "matches",
+                 "alsa_output.usb-GuangZhou_FiiO_Electronics_Co._Ltd_FiiO_K5_Pro-00.playback.0.0"}}},
+    apply_properties = {
+        ["priority.session"] = 10000
+    }
+}
+
 table.insert(alsa_monitor.rules, golden_rule)
+table.insert(alsa_monitor.rules, fix_channels)
+table.insert(alsa_monitor.rules, switch_to_dac)
