bf9e9e0aefbfc99bd8c64ff468ea32d6d4e1ab3c
Vinicius Longaray <vmouralongaray@gmail.com>
2022-07-27 17:10:09 -0300

    Fix treesitter highlights

--- .config/nvim/highlights.lua
+++ .config/nvim/highlights.lua
@@ -13,6 +13,20 @@ M.add = function()
         VirtColumn = {
             fg = "#592929",
         },
+        -- neovim tree-sitter
+        TSBoolean = { fg = "#7dcfff" },
+        TSConstant = { fg = "#e0af68" },
+        TSConstBuiltin = { fg = "#e0af68" },
+        TSConstMacro = { fg = "#e0af68" },
+        TSDanger = { fg = "#1a1b26", bg = "#db4b4b" },
+        TSLabel = { fg = "#7aa2f7" },
+        TSNote = { fg = "#1a1b26", bg = "#0db9d7" },
+        TSOperator = { fg = "#89ddff" },
+        TSTextReference = { fg = "#1abc9c" },
+        TSType = { fg = "#bb9af7" },
+        TSTypeDefinition = { fg = "#7dcfff" },
+        TSTypeQualifier = { fg = "#7dcfff" },
+        TSWarning = { fg = "#1a1b26", bg = "#e0af68" },
     }
 end
 
@@ -57,34 +71,23 @@ M.override = function()
         },
 
         -- These groups are for the neovim tree-sitter highlights.
-        TSBoolean = { fg = "#7dcfff" },
-        TSConstMacro = { fg = "#9d7cd8" },
-        TSDanger = { fg = "#1a1b26", bg = "#db4b4b" },
         TSFuncBuiltin = { fg = "#7aa2f7" }, -- For builtin functions: `table.insert` in Lua.
         TSFunction = { fg = "#7aa2f7" }, -- For function (calls and definitions).
         TSInclude = { fg = "#7dcfff" }, -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
         TSKeyword = { fg = "#9d7cd8" }, -- For keywords that don't fall in previous categories.
         TSKeywordFunction = { fg = "#bb9af7" }, -- For keywords used to define a fuction.
         TSKeywordOperator = { fg = "#9d7cd8" },
-        TSLabel = { fg = "#7aa2f7" }, -- For labels: `label:` in C and `:label:` in Lua.
         TSMethod = { fg = "#7aa2f7" }, -- For builtin functions: `table.insert` in Lua.
         TSNamespace = { fg = "#f7768e" }, -- For identifiers referring to modules and namespaces.
-        TSNote = { fg = "#1a1b26", bg = "#0db9d7" },
-        TSOperator = { fg = "#89ddff" }, -- For any operator: `+`, but also `->` and `*` in C.
         TSParameter = { fg = "#e0af68" }, -- For parameters of a function.
         TSProperty = { fg = "#73daca" }, -- Same as `TSField`.
         TSPunctBracket = { fg = "#a9b1d6" }, -- For brackets and parens.
         TSPunctDelimiter = { fg = "#89ddff" }, -- For delimiters ie: `.`
         TSPunctSpecial = { fg = "#89ddff" }, -- For special punctutation that does not fall in the catagories before.
         TSStringRegex = { fg = "#b4f9f8" }, -- For regexes.
-        TSTextReference = { fg = "#1abc9c" },
-        TSType = { fg = "#bb9af7" }, -- For types.
         TSTypeBuiltin = { fg = "#bb9af7" }, -- For builtin types.
-        TSTypeDefinition = { fg = "#7dcfff" },
-        TSTypeQualifier = { fg = "#7dcfff" },
         TSVariable = { fg = "#c0caf5" }, -- Any variable name that does not have another highlight.
         TSVariableBuiltin = { fg = "#c0caf5" }, -- Variable names that are defined by the languages, like `this` or `self`.
-        TSWarning = { fg = "#1a1b26", bg = "#e0af68" },
     }
 end
 
