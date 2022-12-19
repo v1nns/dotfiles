c482a46c2f4d115a934d91f2b7f1134bb1d89484
Vinicius Longaray <vmouralongaray@gmail.com>
2022-12-19 10:33:40 -0300

    Add keybindings for brightness and improve calendar.sh

--- .local/bin/calendar.sh
+++ .local/bin/calendar.sh
@@ -6,6 +6,11 @@ YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=200 # 193 is minimum possible value
 DATE="$(date +"%a %d %H:%M")"
 
+if ! type "xdotool" >/dev/null; then
+    notify-send "xdotool could not be found, must install it"
+    exit
+fi
+
 case "$1" in
 --popup)
     if [ "$(xdotool getwindowfocus getwindowname)" = "yad-calendar" ]; then
@@ -22,11 +27,10 @@ case "$1" in
     : $((pos_x = X - YAD_WIDTH / 2))
 
     # Y
-    : $((pos_y = HEIGHT - YAD_HEIGHT + BAR_HEIGHT))
+    : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT))
 
-    GTK_THEME="Dracula" yad --calendar --undecorated --fixed --close-on-unfocus\
-        --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
-        --title="yad-calendar" --borders=0 --no-buttons>/dev/null &
+    GTK_THEME="Dracula" yad --calendar --undecorated --fixed --close-on-unfocus --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
+        --title="yad-calendar" --borders=0 --no-buttons >/dev/null &
     ;;
 *)
     echo "$DATE"
