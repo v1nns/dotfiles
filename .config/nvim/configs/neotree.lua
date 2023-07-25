1c6c3470b65478ddd288055c0247450b95f5118f
Vinicius Longaray <vmouralongaray@gmail.com>
2023-07-25 13:20:41 -0300

    Fix neotree config migration

--- .config/nvim/configs/neotree.lua
+++ .config/nvim/configs/neotree.lua
@@ -172,7 +172,7 @@ local options = {
                 ".git",
             },
         },
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
         group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
@@ -259,7 +259,7 @@ local options = {
                 return result
             end,
         },
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
