13f0f87db13b43b5ba00a9fd45407bb36912ba43
Vinicius Longaray <vmouralongaray@gmail.com>
2023-10-25 12:40:26 -0300

    Improve rename_workspace script to show current workspace name

--- .local/bin/rename-workspace.sh
+++ .local/bin/rename-workspace.sh
@@ -2,25 +2,32 @@
 # Author: Vinicius M Longaray
 # Github: @v1nns
 
-# my own path to get custom theme for rofi
-dir="$HOME/.config/rofi/nord"
-rofi_command="rofi -theme $dir/renameworkspace.rasi"
+# custom theme for rofi
+declare -r ROFI_THEME_DIR="$HOME/.config/rofi/nord"
+
+# get info from current workspace
+workspace_info="i3-msg -t get_workspaces"
+eval "$workspace_info"
+
+# filter index and name (if existing) from focused workspace
+index="$($workspace_info | jq -r 'map(select(.focused))[0].num')"
+name="$($workspace_info | jq -r 'map(select(.focused))[0].name' | cut -d ':' -f 2 -s | xargs)"
+
+# format rofi command with theme argument
+rofi_command="rofi -theme $ROFI_THEME_DIR/renameworkspace.rasi"
 
 # open rofi window asking for new name
-input="$($rofi_command -dmenu -lines 0 -p 'New name for this workspace:')"
+input="$($rofi_command -dmenu -lines 0 -p 'New name for this workspace:' -filter $name)"
 
 # if rofi has exitted by Esc, just exit from script
 [[ ${PIPESTATUS[0]} -ne 0 ]] && exit 0
-
-# get number from current workspace
-current_workspace="$(i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].num')"
 
 # check output from rofi
 if [ -z "$input" ]
 then
     # $input is empty, set workspace name only with its number
-    i3-msg rename workspace to "$current_workspace"
+    i3-msg rename workspace to "$index"
 else
     # $input is NOT empty, set workspace to "<number>:$input"
-    i3-msg rename workspace to "$current_workspace:$input"
+    i3-msg rename workspace to "$index:$input"
 fi
