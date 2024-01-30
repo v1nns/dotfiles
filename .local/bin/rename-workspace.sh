e1af092831fdf0ffe51bcd2710b4479c021b59cf
Vinicius Longaray <vmouralongaray@gmail.com>
2024-01-30 12:49:18 -0300

    Increate font size in polybar and change text in rename-workspace.sh

--- .local/bin/rename-workspace.sh
+++ .local/bin/rename-workspace.sh
@@ -17,7 +17,7 @@ name="$($workspace_info | jq -r 'map(select(.focused))[0].name' | cut -d ':' -f 
 rofi_command="rofi -theme $ROFI_THEME_DIR/renameworkspace.rasi"
 
 # open rofi window asking for new name
-input="$($rofi_command -dmenu -lines 0 -p 'New name for this workspace:' -filter $name)"
+input="$($rofi_command -dmenu -lines 0 -p 'Rename workspace:' -filter $name)"
 
 # if rofi has exitted by Esc, just exit from script
 [[ ${PIPESTATUS[0]} -ne 0 ]] && exit 0
