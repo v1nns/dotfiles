288f6ab436624a74185ac5b2c449b03e2d3819e4
Vinicius Longaray <vmouralongaray@gmail.com>
2024-01-31 23:16:04 -0300

    Improve switch-window script

--- .local/bin/switch-window.sh
+++ .local/bin/switch-window.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-rofi -modi combi -combi-modi window -show combi -theme ~/.config/rofi/nord/switchwindow.rasi &
+case $1 in
+--focus) wmfocus --font "Hack Nerd Font Mono":125 --bgcolorcurrent "rgba(30, 30, 30, 0.3)" --fill --print-only | xargs xdotool windowfocus ;;
+--switch) rofi -modi combi -combi-modi window -show combi -theme ~/.config/rofi/nord/switchwindow.rasi & ;;
+*) echo "Invalid option" ;;
+esac
+
