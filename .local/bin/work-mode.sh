5158ff400793c97d61fc7148927ea7c05231342a
Vinicius Longaray <vmouralongaray@gmail.com>
2025-02-10 10:22:02 -0300

    Update work-mode.sh to support hyprland

--- .local/bin/work-mode.sh
+++ .local/bin/work-mode.sh
@@ -1,37 +1,60 @@
 #!/usr/bin/env bash
-# Author: Vinicius M Longaray (@v1nns)
+# Author: Vinicius M. Longaray (@v1nns)
 
 # ---------------------------------------------------------------------------- #
-#              Open program and move it to the right i3 workspace              #
+#                       Move to the correct i3 workspace                       #
+# ---------------------------------------------------------------------------- #
+
+move_i3() {
+  # move it between workspaces
+  i3-msg '[class="'${data_ref[className]}'"] move workspace '${data_ref[workspaceId]}
+  i3-msg 'workspace '${data_ref[workspaceId]}',' \
+    'rename workspace to "'${data_ref[workspaceName]}'",' \
+    'move workspace to output '${data_ref[outputMonitor]}
+}
+
+# ---------------------------------------------------------------------------- #
+#                    Move to the correct hyprland workspace                    #
+# ---------------------------------------------------------------------------- #
+
+move_hyprland() {
+  # move it between workspaces
+  hyprctl dispatch movetoworkspace ${data_ref[workspaceId]}, class:'^('${data_ref[className]}')$'
+  hyprctl dispatch renameworkspace ${data_ref[workspaceId]} ${data_ref[workspaceName]}
+}
+
+# ---------------------------------------------------------------------------- #
+#              Open program and move it to the expected workspace              #
 # ---------------------------------------------------------------------------- #
 
 do_the_magic_with() {
-    local -n data_ref=$1
-    cmd="ps ax | grep ${data_ref[name]} | grep -v grep | wc -l"
+  local -n data_ref=$1
+  cmd="ps ax | grep ${data_ref[name]} | grep -v grep | wc -l"
 
-    # if not running yet
+  # if not running yet
+  appCount=$(eval "$cmd")
+  if [ "$appCount" -le "0" ]; then
+    # start application detached
+    (${data_ref[command]} &)
+  fi
+
+  # wait until program starts running wild
+  while [ "$appCount" -le "0" ]; do
+    sleep 1
     appCount=$(eval "$cmd")
-    if [ "$appCount" -le "0" ]; then
-        # start application detached
-        (${data_ref[command]} &)
-    fi
+  done
 
-    # wait until program starts running wild
-    while [ "$appCount" -le "0" ]; do
-        sleep 1
-        appCount=$(eval "$cmd")
-    done
+  # workaround for slow loading screen =(
+  if [[ "${data_ref[className]}" == "teams" ]]; then
+    sleep 3
+  fi
 
-    # workaround for slow loading screen =(
-    if [[ "${data_ref[name]}" == "teams" ]]; then
-        sleep 3
-    fi
+  if [[ "$DESKTOP_SESSION" == "i3" ]]; then
+    move_i3 data_ref
+  elif [[ "$DESKTOP_SESSION" == "hyprland" ]]; then
+    move_hyprland data_ref
+  fi
 
-    # move it between workspaces
-    i3-msg '[class="'${data_ref[className]}'"] move workspace '${data_ref[workspaceId]}
-    i3-msg 'workspace '${data_ref[workspaceId]}',' \
-        'rename workspace to "'${data_ref[workspaceName]}'",' \
-        'move workspace to output '${data_ref[outputMonitor]}
 }
 
 # ---------------------------------------------------------------------------- #
@@ -59,16 +82,16 @@ choose_the_red_pill() {
 #                  Confirmation about executing the work mode                  #
 # ---------------------------------------------------------------------------- #
 
-dir="~/.config/rofi/nord"
+dir="~/.config/rofi/tokyo"
 
 confirm_work_mode() {
-    rofi -dmenu -i -no-fixed-num-lines -p "Enter the matrix? " -theme $dir/confirm.rasi
+  rofi -dmenu -i -no-fixed-num-lines -p "Enter the matrix? " -theme $dir/confirm.rasi
 }
 
 ans=$(confirm_work_mode &)
 if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-    choose_the_red_pill
+  choose_the_red_pill
 else
-    # nah, just take the blue one
-    exit 0
+  # nah, just take the blue one
+  exit 0
 fi
