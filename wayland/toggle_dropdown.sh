ed3863ab73e0650b0007cfb609646bcfc4910c8e
Vinicius M. Longaray <vmouralongaray@gmail.com>
2026-04-26 22:39:13 -0300

    Update hyprland to match new syntax

--- wayland/toggle_dropdown.sh
+++ wayland/toggle_dropdown.sh
@@ -5,10 +5,9 @@
 # Toggle visibility of dropdown terminal and resize it based on the current focused monitor
 
 # hacky method to get cursor centered
-hyprctl dispatch focusmonitor +1
-hyprctl dispatch focusmonitor -1
+# hyprctl dispatch focusmonitor +1
+# hyprctl dispatch focusmonitor -1
 
-#hyprctl dispatch resizeactive exact 55% 60%
 hyprctl dispatch resizewindowpixel exact 55% 60%, 'title:^(dropdown-terminal)$'
 
 hyprctl dispatch togglespecialworkspace dropdown
